0ec589d1b546fbbe05948f518c6e39675e388e5d
Alex Zhang <1309701864@qq.com>
2021-02-05 18:47:10 -0500

    add tutorial

---
+++ Learning WeApp Day 1/Day 1/pages/index/index.js
@@ -0,0 +1,9 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    count: 0
+  },
+  // Add you code here
+})
---
+++ Learning WeApp Day 3/Day 3/pages/index/index.js
@@ -0,0 +1,13 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    cells: ['(', ')', '%', 'CE', 7, 8, 9, '/', 4, 5, 6, 'x', 1, 2, 3, '-', '.', 0, '=', '+'],
+    answer: 0
+  },
+  click(e) {
+    console.log(e)
+  }
+})
